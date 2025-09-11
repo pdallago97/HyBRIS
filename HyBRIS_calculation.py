914f7261ee2aef775528ad5f7e467d9e074769f5
pdallago97 <paolo.dallago@wur.nl>
2025-09-11 10:33:22 +0200

    Included examples files and example code to reproduce TimeSeries figures in manuscript

--- HyBRIS_calculation.py
+++ HyBRIS_calculation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy.signal import find_peaks
+import matplotlib.pyplot as plt
 
 def normalize_percentiles(data, lower_percentile=0.02, upper_percentile=0.98):
     """
@@ -24,6 +25,189 @@ def normalize_percentiles(data, lower_percentile=0.02, upper_percentile=0.98):
     normalized_data = np.clip(normalized_data, 0, 1)
     
     return normalized_data
+
+
+def add_vis_radar(df):
+    """
+    Add Radar Vegetation Indices (RVI and others) to the dataframe.
+    
+    Args:
+        df (pandas.DataFrame): Input dataframe with required radar bands (VH, VV).
+        
+    Returns:
+        pandas.DataFrame: DataFrame with added RVI, VH_VV, VV_VH, and RI2 columns.
+    """
+    # Work on a copy to avoid SettingWithCopyWarning
+    df = df.copy()
+
+    # Check for required bands
+    required_bands = ['VH', 'VV']
+    for band in required_bands:
+        if band not in df.columns:
+            raise ValueError(f"Missing required band: {band}")
+    
+    # Ensure all bands are numeric
+    for band in required_bands:
+        df[band] = pd.to_numeric(df[band], errors='coerce')
+    
+    # # Convert dB to linear scale
+    # vv_lin = 10 ** (df['VV'] / 10)
+    # vh_lin = 10 ** (df['VH'] / 10)
+
+    # Calculate Radar Vegetation Indices
+    with np.errstate(divide='ignore', invalid='ignore'):
+        df['RVI'] = normalize_percentiles((4 * df['VH']) - (df['VV'] + df['VH']))
+        #df["RVI"] = normalize_percentiles(4 * vh_lin / (vv_lin + vh_lin))
+        df['VH_VV'] = normalize_percentiles(df['VH'] - df['VV'])
+        df['VV_VH'] = normalize_percentiles(df['VV'] - df['VH'])
+        df['RI2'] = normalize_percentiles((df['VV'] - df['VH']) / (df['VV'] + df['VH']))
+    
+    return df
+
+
+def add_vis(df):
+    """
+    Add Vegetation Indices (VIs) to the dataframe.
+    
+    Args:
+        df (pandas.DataFrame): Input dataframe with required bands (B2, B3, B4, B8, B11, B12).
+        
+    Returns:
+        pandas.DataFrame: DataFrame with added NDVI, BSI, NDTI, NDSI, and NDWI columns.
+    """
+    # Work on a copy to avoid SettingWithCopyWarning
+    df = df.copy()
+
+    # Ensure no division by zero or invalid operations
+    with np.errstate(divide='ignore', invalid='ignore'):
+        # NDVI
+        df['NDVI'] = normalize_percentiles((df['B8'] - df['B4']) / (df['B8'] + df['B4']))
+        
+        # BSI
+        df['BSI'] = normalize_percentiles(((df['B11'] + df['B4']) - (df['B8'] + df['B2'])) / 
+                                          ((df['B11'] + df['B4']) + (df['B8'] + df['B2'])))
+        
+        # NDTI
+        df['NDTI'] = normalize_percentiles((df['B11'] - df['B12']) / (df['B11'] + df['B12']))
+        
+        # NDSI
+        df['NDSI'] = normalize_percentiles((df['B3'] - df['B11']) / (df['B3'] + df['B11']))
+        
+        # NDWI
+        df['NDWI'] = normalize_percentiles((df['B8'] - df['B12']) / (df['B8'] + df['B12']))
+
+        #EVI
+        df['EVI'] = 2.5 * (df['B8'] - df['B4']) / ((df['B8'] + 6 * df['B4'] - 7.5 * df['B2']) + 1)
+
+    return df
+
+def openSentinel2file(filename, bandsused):
+    """
+    Opens a Sentinel-2 CSV file, processes it, and returns a DataFrame.
+    
+    Parameters:
+    filename (str): The path to the CSV file.
+    column_names (list): The list of column names for the DataFrame.
+    bandsused (list): The list of band columns to be processed.
+    add_vis (function): A function to add visualization columns to the DataFrame.
+    
+    Returns:
+    pd.DataFrame: The processed DataFrame.
+    """
+    # Define column names
+    column_names = ['date', 'ID'] + bandsused
+
+    try:
+        # Open the CSV file and assign column names
+        df = pd.read_csv(filename, header=None, names=column_names)
+        
+        # Replace -9999 with NaN
+        df.replace(-9999, pd.NA, inplace=True)
+        #remove NA
+        df = df.dropna()
+        
+        # Convert date column to datetime
+        df['date'] = pd.to_datetime(df['date'])
+        
+        # Ensure bandsused columns are numeric and divide by 10,000
+        df[bandsused] = df[bandsused].apply(pd.to_numeric, errors='coerce')  # Convert to numeric, coercing errors to NaN
+        df[bandsused] = df[bandsused].div(10000)
+        
+        # Merge observation which happen on the same date by taking the maximum value
+        df = df.groupby('date', as_index=False).max()
+
+        return df
+
+    except FileNotFoundError:
+        print(f"Error: File {filename} not found.")
+        exit()
+
+def openSentinel1file(filename, bandsusedS1):
+    """
+    Opens a Sentinel-1 CSV file, processes it, and returns a DataFrame.
+    
+    Parameters:
+    filename (str): The path to the CSV file.
+    bandsused (list): The list of band columns to be processed.
+    add_vis (function): A function to add visualization columns to the DataFrame.
+    
+    Returns:
+    pd.DataFrame: The processed DataFrame.
+    """
+    # Define column names
+    column_names = ['date', 'ID', 'orbit', 'orbitNumber'] + bandsusedS1
+
+    # Open the CSV file and assign column names
+    try:
+        df = pd.read_csv(filename, header=None, names=column_names)
+
+        # Replace -9999 with NaN
+        df.replace(-9999, pd.NA, inplace=True)
+        #remove NA
+        df = df.dropna()
+
+        # Convert date column to datetime
+        df['date'] = pd.to_datetime(df['date'])
+        
+        # Ensure bandsused columns are numeric
+        df[bandsusedS1] = df[bandsusedS1].apply(pd.to_numeric, errors='coerce')  # Convert to numeric, coercing errors to NaN
+
+        # Merge observation which happen on the same date by taking the max value
+        df = df.groupby('date', as_index=False).max()
+        
+        return df
+    
+    except FileNotFoundError:
+        print(f"Error: File {filename} not found.")
+        exit()
+
+def selectOrbit(df, selectMostPresent=False, selectHighestAngle=False):
+    """
+    Filters the Sentinel-1 dataframe by orbit type and orbit number.
+
+    Parameters:
+    df (pd.DataFrame): The Sentinel-1 time series dataframe.
+    selectMostPresent (bool): If True, returns only the orbitNumber with the most observations.
+    selectHighestAngle (bool): If True, returns only the orbitNumber with the highest average angle.
+
+    Returns:
+    dict (if both flags are False) or pd.DataFrame (if one flag is True)
+    """
+    grouped = df.groupby(['orbit', 'orbitNumber'])
+
+    if selectMostPresent:
+        # Orbit with the most observations
+        most_present_orbit = max(grouped.groups, key=lambda x: len(grouped.get_group(x)))
+        return grouped.get_group(most_present_orbit)
+
+    if selectHighestAngle:
+         # Orbit with the highest mean angle
+        highest_angle_orbit = max(grouped.groups, key=lambda x: grouped.get_group(x)['angle'].mean())
+        return grouped.get_group(highest_angle_orbit)
+
+    # Return all orbit combinations as dictionary
+    return {f"{orbit}_{orbitNum}": group for (orbit, orbitNum), group in grouped}
+
 
 def daily_index_with_contributions(s2df, s1df, maxDiff=30, bandS2='NDVI', bandS1='RVI'):
     """
@@ -444,3 +628,240 @@ def add_predictions(hybris, predictions):
     )
 
     return predictions
+
+def merge_with_GT(merged, ground_truth): 
+    # Prep inputs
+    merged = merged.copy().rename(columns={"date": "Date"})
+    ground_truth = ground_truth.copy()
+
+
+    merged["Farm"] = ground_truth["Farm"].unique()[0]
+    merged["Country"] = ground_truth["Country"].unique()[0]
+
+    # Add event types
+    type_map = {1: "obs_tillage", 2: "obs_sowing", 3: "obs_harvest"}
+    ground_truth["obs_type"] = ground_truth["Ti.1_So.2_Ha.3"].map(type_map)
+
+    # All combinations of merged dates and ground truth events on same date
+    merged = merged.merge(
+        ground_truth[["Date", "obs_type", "Crop", "Crop.type", "Method_EN", "Ti.1_So.2_Ha.3"]],
+        on="Date",
+        how="left"
+    )
+    return merged
+
+def filter_by_date(df, start_date, end_date, date_column='date'):
+    """
+    Filters a DataFrame based on a date range.
+
+    Parameters:
+        df (pd.DataFrame): DataFrame containing a 'date' column.
+        start_date (str or pd.Timestamp): Start date (inclusive).
+        end_date (str or pd.Timestamp): End date (inclusive).
+
+    Returns:
+        pd.DataFrame: Filtered DataFrame.
+    """
+    df[date_column] = pd.to_datetime(df[date_column])  # Ensure 'date' is datetime type
+    return df[(df[date_column] >= pd.to_datetime(start_date)) & (df[date_column] <= pd.to_datetime(end_date))]
+
+######### VISUALIZATION #########
+
+def plot_hybris(hybris, plot_tillages = True, plot_dormant=False, add_groundtruth = True, add_pred_sow_harv=True, ax = None):
+    
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(12, 5))
+        
+    # Plot smoothed time series
+    ax.plot(hybris["Date"], hybris["daily_index_smooth"], label="Smoothed", color="brown")
+
+    # Plot raw time series with transparency
+    if "daily_index" in hybris.columns:
+        ax.plot(hybris["Date"], hybris["daily_index"], label="Unsmoothed", color="brown", alpha=0.3)
+
+    
+        # Handle tillage points
+        if plot_tillages:
+            if "pred_tillage" in hybris["pred_type"].unique():
+                tillage_all = hybris[hybris["pred_type"] == "pred_tillage"]
+            
+                #Plot other tillages
+                ax.scatter(tillage_all["Date"], tillage_all["Value"],
+                        label="Predicted tillage",
+                        color="brown", marker="s", zorder=5)
+    
+    # Add groundtruth events as vertical lines
+    if add_groundtruth:
+        if hybris is not None:
+            category_map = {"obs_tillage": 'Tillage',
+                            'obs_sowing': 'Sowing',
+                            'obs_harvest': 'Harvest'
+                               }
+            category_colors = {"obs_tillage": 'brown',
+                                'obs_sowing': 'green',
+                                'obs_harvest': 'black'
+                                  }
+            category_style = {"obs_tillage": '--',
+                               'obs_sowing': '-.',
+                               'obs_harvest': '-.'
+                                 }
+
+            added_labels = set()
+            for category, color in category_colors.items():
+                dates_to_mark = hybris.loc[hybris['obs_type'] == category, 'Date']
+                for date in dates_to_mark:
+                    label = category_map[category] if category not in added_labels else None
+                    style = category_style[category]
+                    ax.axvline(date, color=color, linestyle=style, alpha=0.8, label=label)
+                    added_labels.add(category)
+
+    # Plot phenological points if provided
+    if add_pred_sow_harv:
+        if hybris is not None:
+            type_colors = {
+                "pred_sowing": "green",
+                "peak": "red",
+                "pred_harvest": "black"
+            }
+            type_style = {"pred_sowing": 'o', 'peak': '^', 'pred_harvest': 'o'}
+            lebel_typ = {"pred_sowing": 'Predicted sowing', 'peak': 'Peak', 'pred_harvest': 'Predicted harvest'}
+            # Handle normal phenology (sowing, peak, harvest)
+            for typ in ["pred_sowing", "peak", "pred_harvest"]:
+                group = hybris[hybris["pred_type"] == typ]
+                ax.scatter(group["Date"], group["Value"],
+                        label=lebel_typ[typ],
+                        color=type_colors.get(typ, "blue"),
+                        marker=type_style[typ],
+                        zorder=5)
+            
+    # Plot dormant periods (from harvest to next sowing)
+    if plot_dormant:
+        ymin, ymax = ax.get_ylim()          # vertical span of the plot
+        ax.fill_between(hybris["Date"],
+                        -0, 1,         # shade full height
+                        where=~hybris["in_growing_season"],
+                        color="gray", alpha=0.3,
+                        label="Dormant period",
+                        step="post")        # keeps the shading block‑wise
+
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    ax.set_title("")
+    ax.legend(
+    loc='upper left',        # Position relative to bbox
+    bbox_to_anchor=(1.0, 0.8) # (x, y) coordinates relative to the axes
+    )
+    ax.grid(False)
+   
+    return ax
+
+
+def plot_time_series(values, dates, groundtruth=None, season_boundaries=None,
+                      tillages = None, tillage_windows = None, dormant_periods = None, color='gray',
+                        title="Time Series", add = False, show = True, alpha = 1, marker = None,
+                        ax = None):
+    """
+    Plots a time series and optionally adds vertical lines for events in the groundtruth DataFrame.
+    It also overlays detected Start of Season (SOS), End of Season (EOS), and Peak points.
+
+    Parameters:
+    -----------
+    values : array-like
+        The values of the time series.
+
+    dates : array-like
+        The dates corresponding to the time series.
+
+    groundtruth : pd.DataFrame, optional
+        A DataFrame with event dates and categories.
+
+    season_boundaries : pd.DataFrame, optional
+        A DataFrame with 'SOS Date', 'EOS Date', and 'Peak Date' columns.
+
+    color : str, optional (default='blue')
+        Color of the time series line.
+
+    title : str, optional (default="Time Series")
+        Title of the plot.
+
+    Returns:
+    --------
+    None
+    """
+    # Create new axes only if add is False and no ax is passed
+    if add is False and ax is None:
+        fig, ax = plt.subplots(figsize=(12, 5))
+
+    # # If ax is still None (edge case), create one
+    # if ax is None:
+    #     fig, ax = plt.subplots(figsize=(12, 5))
+
+    mask = ~np.isnan(values) & ~pd.isnull(dates)
+    values = np.array(values)[mask]
+    dates = np.array(dates)[mask]
+    sorted_idx = np.argsort(dates)
+    values = values[sorted_idx]
+    dates = dates[sorted_idx]
+
+    ax.plot(dates, values, label=title, color=color, alpha=alpha, marker=marker)
+
+    # Add groundtruth events as vertical lines
+    if groundtruth is not None:
+        category_map = {1: 'Tillage', 2: 'Sowing', 3: 'Harvest'}
+        category_colors = {1: 'brown', 2: 'green', 3: 'black'}
+        category_style = {1: '--', 2: '-.', 3: '-.'}
+
+        added_labels = set()
+        for category, color in category_colors.items():
+            dates_to_mark = groundtruth.loc[groundtruth['Ti.1_So.2_Ha.3'] == category, 'Date']
+            for date in dates_to_mark:
+                label = category_map[category] if category not in added_labels else None
+                style = category_style[category]
+                ax.axvline(date, color=color, linestyle=style, alpha=0.8, label=label)
+                added_labels.add(category)
+
+    # Add season boundaries (SOS, EOS, Peaks)
+    if season_boundaries is not None:
+        ax.scatter(season_boundaries['SOS Date'], season_boundaries['SOS Value'], 
+                    color='green', marker='o', label='Predicted sowing')
+        ax.scatter(season_boundaries['Peak Date'], season_boundaries['Peak Value'], 
+                    color='red', marker='^', label='Peak')
+        ax.scatter(season_boundaries['EOS Date'], season_boundaries['EOS Value'], 
+                    color='black', marker='o', label='Predicted harvest')
+        # Add season boundaries (SOS, EOS, Peaks)
+    if tillages is not None:
+        ax.scatter(tillages['Date'], tillages['Value'], 
+                    color='brown', marker='o', label='Predicted tillage')
+    if tillage_windows is not None:
+        # Convert 'window_start' and 'window_end' to datetime
+        tillage_windows.loc[:,'window_start'] = pd.to_datetime(tillage_windows['window_start'])
+        tillage_windows.loc[:,'window_end'] = pd.to_datetime(tillage_windows['window_end'])
+  
+        # Iterate over each row in the dataframe and add vertical lines and shaded areas
+        for index, row in tillage_windows.iterrows():
+            window_start = row['window_start']
+            window_end = row['window_end']
+            
+            # Add vertical lines at window start and window end
+            ax.axvline(window_start, color='red', linestyle='--', lw=2, label='Window Start' if index == 0 else "")
+            ax.axvline(window_end, color='blue', linestyle='--', lw=2, label='Window End' if index == 0 else "")
+            
+            # Add shaded area between window start and window end
+            ax.axvspan(window_start, window_end, color='gray', alpha=0.3) 
+
+    # Plot shaded dormant periods  
+    if dormant_periods is not None:
+        for i, row in dormant_periods.iterrows():
+            eos = pd.to_datetime(row['Harvest Date'])
+            sos = pd.to_datetime(row['Sowing Date'])
+            ax.axvspan(eos, sos, color="gray", alpha=0.3, label="Dormant Period" if i == 0 else "")
+
+    ax.legend(loc='upper left', bbox_to_anchor=(1.0, 0.5)) # (x, y) coordinates relative to the axes)
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    ax.set_title("")
+    ax.grid(False)
+    if show:
+        plt.show()
+
+    return ax
---
+++ HyBRIS_example.py
@@ -0,0 +1,121 @@
+from HyBRIS_calculation import *
+
+#load data
+
+#Ground truth data with sowing and harvest dates
+field = pd.read_csv('GroundTruth_example.csv')
+field['Date'] = pd.to_datetime(field['Date']) #make sure the date column is in datetime format
+
+#paths to example data files, the Sentinel 1 and 2 time series
+s2_path = 'Sentinel2_example.csv'
+s1_path = 'Sentinel1_example.csv'
+
+# Open Sentinel-2, remove NA, merge observation on the same day, and add Vegetation indices
+bandsused = ['B1', 'B2', 'B3', 'B4','B5', 'B6', 'B7', 'B8', 'B8A', 'B11', 'B12']
+bandsusedS1 = ['VV', 'VH', 'angle']
+
+s2 = openSentinel2file(s2_path, bandsused)
+s2 = add_vis(s2)
+
+#and Sentinel-1 files, remove NA, merge observation on the same day, select most present orbit, and add Vegetation indices
+s1 = openSentinel1file(s1_path, bandsusedS1)
+
+#Split ascending and descending orbits
+s1_des = s1[s1['orbit'] == 'DESCENDING']  # Select only descending orbits
+s1_asc = s1[s1['orbit'] == 'ASCENDING']  # Select only ascending orbits
+
+# Select most present orbit and add radar vegetation index
+s1_des = add_vis_radar(selectOrbit(s1_des, selectMostPresent=True))
+s1_asc = add_vis_radar(selectOrbit(s1_asc, selectMostPresent=True))
+
+s1 = pd.concat([s1_des, s1_asc])
+
+#specify bands for S1 and S2 fusion
+s2_band = "BSI"
+s1_band = "VV_VH"
+
+#Fuse S1 and S2 time series based on given bands
+pp = daily_index_with_contributions(s2,s1, maxDiff= 30, bandS2= "BSI", bandS1= "VV_VH")
+pp['daily_index'] = 1 - pp['daily_index']
+
+#Fuse S1 and S2 time series based on given bands(similar to daily_index_with_contributions, but specifically for HyBRIS)
+hybris = calculate_hybris(s1, s2)
+
+#Find maxima and minima in the fused time series
+maxima = find_maxima(hybris) #Peaks of seasons
+minima = find_minima(hybris, distanceTillages=30, prominenceTillages=(0,1), height= None) #Sowing, harvest, tillage
+
+#Identify growing seasons based on predicted minima
+g_seasons = growing_seasons(maxima, minima) #assign sowing and harvest dates to a peak of season
+
+#Filter out small seasons (less than 60 days)
+g_seasons = filter_small_seasons(g_seasons)
+
+predictions = add_tillages(g_seasons, minima)
+
+#Merge with predictions
+hybris = add_predictions(hybris, predictions)
+
+#Merge with ground truth data
+hybris = merge_with_GT(hybris, field) #combine everything in one long dataframe
+
+#define start and end date for examples
+start_date = pd.to_datetime("2017-01-01")
+end_date = pd.to_datetime("2025-01-01")
+
+#Filter all data to the same time period for easier plotting
+field_example = filter_by_date(field, start_date=start_date, end_date=end_date, date_column="Date")
+s2_example = filter_by_date(s2, start_date=start_date, end_date=end_date)
+s1_example = filter_by_date(s1, start_date=start_date, end_date=end_date)
+pp_example = filter_by_date(pp, start_date=start_date, end_date=end_date)
+minima_example = filter_by_date(minima, start_date=start_date, end_date=end_date, date_column="Date")
+
+####PLOT FIGURE WITH 1 PANELS
+fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(11.5, 4.1), sharex=True)
+plot_hybris(hybris[(hybris["Date"] > start_date) & (hybris["Date"] < end_date)],
+            add_pred_sow_harv = False,
+            plot_tillages=False,
+            plot_dormant=True,
+            add_groundtruth=True,
+            ax = axes)
+
+plt.tight_layout()
+plt.show()
+
+####PLOT FIGURE WITH 4 PANELS
+fig, axes = plt.subplots(nrows=4, ncols=1, figsize=(8.2, 11.5), sharex=True)
+# Invert Hybris index
+pp_example['daily_index'] = 1 - pp_example['daily_index']
+
+# --- First panel: S2, S1, HYBRIS
+plot_time_series(s2_example[s2_band], s2_example['date'], color='red', title=s2_band,
+                 show=False, alpha=0.2, marker='o', ax=axes[0])
+plot_time_series(s1_example[s1_band], s1_example['date'], color='purple', title="VV/VH",
+                 add=True, show=False, alpha=0.2, marker='o', ax=axes[0])
+plot_time_series(pp_example['daily_index'], pp_example['date'], color='brown', title="Xd",
+                 add=True, ax=axes[0], show=False)
+
+# Restore original HYBRIS index if needed
+pp_example['daily_index'] = 1 - pp_example['daily_index']
+
+# --- Second panel: NDVI, VH/VV, HYBRIS
+plot_time_series(s2_example["NDVI"], s2_example['date'], color='green', title="NDVI",
+                 show=False, alpha=0.2, marker='o', ax=axes[1])
+plot_time_series(s1_example["VH_VV"], s1_example['date'], color='blue', title="VH/VV",
+                 add=True, show=False, alpha=0.2, marker='o', ax=axes[1])
+plot_time_series(pp_example['daily_index'], pp_example['date'], color='brown',
+                 title="HYBRIS unsmoothed", add=True, ax=axes[1], show=False)
+
+# --- Third panel: plot_hybris without groundtruth
+plot_hybris(hybris[(hybris["Date"] > start_date) & (hybris["Date"] < end_date)],
+            plot_tillages=True, plot_dormant=True, add_groundtruth=False, ax=axes[2])
+
+# --- Fourth panel: plot_hybris with groundtruth
+plot_hybris(hybris[(hybris["Date"] > start_date) & (hybris["Date"] < end_date)], add_pred_sow_harv = False,
+            plot_tillages=False, plot_dormant=False, add_groundtruth=True, ax=axes[3])
+
+# Optional: tighten layout
+plt.tight_layout()
+plt.show()
+
+
